15534821719c7293c894070b5cb73d0424f24388
Deepak <2021mc13de@mitsgwl.ac.in>
2026-03-11 16:36:20 +0530

    Icon Dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,8 @@ android {
 }
 
 dependencies {
+    implementation("androidx.compose.material:material-icons-extended:1.7.8")
+    implementation("androidx.compose.material3:material3:1.4.0")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
     implementation(libs.androidx.core.ktx)

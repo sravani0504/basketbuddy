d89222c831567ac39e703f911774d50018e0170e
Abhishek <abhishek55ro@gmail.com>
2026-03-12 12:05:28 +0530

    changes in alignment of summary screen

--- app/src/main/java/com/gladiator/BasketBuddy/view/composable/SummaryScreen.kt
+++ app/src/main/java/com/gladiator/BasketBuddy/view/composable/SummaryScreen.kt
@@ -1,6 +1,5 @@
 package com.gladiator.BasketBuddy.view.composable
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -16,28 +15,28 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.gladiator.BasketBuddy.model.Item
 
-
 @Composable
 fun ItemSummaryRow(
     index: Int,
     item: Item
 ){
-    Column(modifier = Modifier.fillMaxWidth().padding(vertical = 10.dp, horizontal = 16.dp)) {
-        Text(text = "$index. ${item.itemName} - ${item.quantity}", fontSize = 18.sp, fontWeight = FontWeight.Medium)
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .padding(vertical = 10.dp, horizontal = 16.dp)) {
+        Text(text = "$index. ${item.itemName} - ${item.quantity}", fontSize = 24.sp, fontWeight = FontWeight.Bold)
         Text(
-            text = item.itemDescription,
+            text = item.itemDescription, fontWeight = FontWeight.Medium,
             fontSize = 14.sp,
             modifier = Modifier.padding(start = 12.dp)
         )
     }
 }
 
-
 @Composable
-fun ItemSummaryList(items: List<Item>){
-    LazyColumn(modifier = Modifier.fillMaxSize().padding(top = 10.dp)) {
+fun ItemSummaryList(items: List<Item>, modifier: Modifier = Modifier){
+    LazyColumn(modifier = modifier.padding(top = 10.dp)) {
         itemsIndexed(items){
-            index, item ->
+                index, item ->
             ItemSummaryRow(
                 index=index+1,
                 item=item
@@ -46,22 +45,21 @@ fun ItemSummaryList(items: List<Item>){
     }
 }
 
-
 @Composable
-fun ItemSummaryScreen(items:List<Item>){
-    Column (modifier = Modifier.fillMaxSize()){
-        ItemSummaryList(items)
-    }
+fun ItemSummaryScreen(items: List<Item>, modifier: Modifier = Modifier){
+    ItemSummaryList(items, modifier = modifier)
 }
 
 @Preview(showBackground = true)
 @Composable
 fun PreviewSummary(){
-    val items=listOf(Item("Milk","1 litre packet",2),
-        Item("Bread","Whole wheat",1))
-    Column (modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.SpaceBetween){
+    val items = listOf(
+        Item("Milk", "1 litre packet", 2),
+        Item("Bread", "Whole wheat", 1)
+    )
+    Column(modifier = Modifier.fillMaxSize()) {
         TopBar(title = "Items Summary", onBackClick = {})
-        ItemSummaryScreen(items)
+        ItemSummaryScreen(items, modifier = Modifier.weight(1f))
         BasketBuddyBottomNav()
     }
 }

364ba21c35c99fb1b8d9fd672f64c8ee82d585a4
Akshita <akshitak2103@gmail.com>
2026-03-12 19:16:01 +0530

    Item Summary Screen UI Update

--- app/src/main/java/com/gladiator/BasketBuddy/view/composable/SummaryScreen.kt
+++ app/src/main/java/com/gladiator/BasketBuddy/view/composable/SummaryScreen.kt
@@ -1,5 +1,7 @@
 package com.gladiator.BasketBuddy.view.composable
 
+import android.R
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -7,9 +9,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -25,27 +31,38 @@ fun ItemSummaryRow(
 ){
     Column(modifier = Modifier
         .fillMaxWidth()
-        .padding(vertical = 10.dp, horizontal = 16.dp)) {
-        Text(text = "$index. ${item.itemName} - ${item.quantity}", fontSize = 24.sp, fontWeight = FontWeight.Bold)
+        .padding(vertical = 12.dp, horizontal = 16.dp)) {
+        Text(text = "$index. ${item.itemName} - ${item.quantity}",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFF5A3E2B)
+        )
         Text(
             text = item.itemDescription, fontWeight = FontWeight.Medium,
             fontSize = 14.sp,
+            color = Color.Gray,
             modifier = Modifier.padding(start = 12.dp)
         )
     }
 }
 
-
 @Composable
 fun ItemSummaryList(items: List<Item>, modifier: Modifier = Modifier){
 
-    LazyColumn(modifier = modifier.padding(top = 10.dp)) {
+    LazyColumn(modifier = modifier.padding(top = 12.dp)) {
         itemsIndexed(items){
                 index, item ->
-            ItemSummaryRow(
-                index=index+1,
-                item=item
-            )
+            Card (modifier = Modifier.fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 6.dp),
+                shape = RoundedCornerShape(14.dp),
+                elevation = CardDefaults.cardElevation(6.dp),
+                colors = CardDefaults.cardColors(containerColor = Color.White)
+            ){
+                ItemSummaryRow(
+                    index=index+1,
+                    item=item
+                )
+            }
         }
     }
 }
@@ -54,9 +71,10 @@ fun ItemSummaryList(items: List<Item>, modifier: Modifier = Modifier){
 @Composable
 fun ItemSummaryScreen(items: List<Item>, modifier: Modifier = Modifier, navController: NavController){
 
-    Column(Modifier.fillMaxSize()) {
-        TopBar(title = "Items Summary", onBackClick = {})
-        ItemSummaryList(items, modifier = modifier)
+    Column(Modifier.fillMaxSize().background(Color(0xFFF5EBDD))
+    ) {
+        TopBar(title = "Items Summary", onBackClick = {navController.popBackStack()})
+        ItemSummaryList(items, modifier = modifier.weight(1f))
         BasketBuddyBottomNav(navController)
     }
 }
